413ac5319eb684bcf564b54414fec6e0e8cc96ba
David Correa <david.correav@gmail.com>
2022-02-12 23:33:54 -0500

    Initial release with hero

--- dcorrea/src/components/layout/Navbar.jsx
+++ dcorrea/src/components/layout/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({ title }) => {
           </Link>
         </div>
         <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
+          <div className="flex justify-end gap-2">
             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
             </Link>
--- dcorrea/src/components/pages/Home.jsx
+++ dcorrea/src/components/pages/Home.jsx
@@ -1,49 +1,47 @@
 import React from "react";
 import guyChilling from "../../assets/guy-chilling.png";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   const phrases = [
-    "is a Web Developer",
-    "Loves to read",
+    "is a Web Developer \n",
+    "loves to read \n",
+    "believes in you \n",
+    "loves Flexbox and Grid",
+    "needs some coffee",
     "thinks you are pretty cool",
     "cooks a mean lasagna",
-    "Loves Flexbox and Grid",
-    "is pretty good at Wordpress",
-    "needs some Coffee",
+    "knows Wordpress",
   ];
-  window.onload = () => {
-    setInterval(() => {
-      const descriptor = document.getElementById("descriptor");
-      descriptor.classList.add("descriptorAnimation");
-      descriptor.textContent =
-        phrases[Math.floor(Math.random() * phrases.length)];
-      setTimeout(() => {
-        descriptor.classList.remove("descriptorAnimation");
-      }, 1000);
-    }, 5000);
-  };
+
   return (
-    <section className="hero min-h-screen">
-      <div className="flex-col hero-content lg:flex-row-reverse gap-12">
+    <section className="hero min-h-screen mt-2">
+      <div className="flex flex-col justify-start align-center z-0 max-w-7xl m-4 p-1 lg:flex-row-reverse lg:gap-12">
         <img
           src={guyChilling}
           alt="Guy sitting with a laptop and a cup of coffee"
-          className="max-w-sm text-center"
+          className="max-w-xs text-center lg:max-w-md"
         />
-        <div className="rowdies text-left">
-          <h1>
-            <span className="text-9xl block px-4 py-2">David</span>
-            <span
-              className="block text-9xl bg-primary text-base-100 px-4 py-2"
-              style={{ width: "42rem" }}
-            >
-              Correa
-            </span>
-          </h1>
-          <div className="">
-            <span className="block text-5xl px-4 py-2" id="descriptor">
-              is a Web Developer
-            </span>
+        <div>
+          <div className="rowdies text-left">
+            <h1>
+              <span className="text-7xl w-full block px-4 py-2 lg:w-max lg:text-9xl">
+                David
+              </span>
+              <span className="block text-7xl w-full bg-primary text-base-100 py-2 px-4 lg:pl-4 lg:pr-20 lg:text-9xl lg:w-max lg:p">
+                Correa
+              </span>
+            </h1>
+          </div>
+          <div className="max-w-xs rowdies text-3xl pl-4 h-10 lg:text-5xl lg:max-w-lg">
+            <Typewriter
+              className="max-w-sm"
+              options={{
+                strings: phrases,
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </div>
         </div>
       </div>
